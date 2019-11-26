f9760ed3d3767b0f4e64526bf5e79397955e2cec
ZhangJiChu <1339446165@qq.com>
2019-11-26 22:32:25 +0800

    使用volatile不能中断线程的场景

--- src/stopthreads/volatiledemo/WrongWayVolatileCannotStop.java
+++ src/stopthreads/volatiledemo/WrongWayVolatileCannotStop.java
@@ -5,6 +5,8 @@ import java.util.concurrent.BlockingQueue;
 
 //使用一个volatile修饰的变量来中断线程具有局限性，
 //譬如当这里的生产者线程阻塞在storage.put()时，程序将不会运行到判断变量canceled代码的位置，因而线程将不会中断。
+//可以用interrupt通知中断，
+//可以响应中断的会导致的方法 Object wait,Thread sleep,Thread join,BlockingQueue take/put,Lock lockInterruptibly
 public class WrongWayVolatileCannotStop {
     public static void main(String[] args) throws InterruptedException {
         ArrayBlockingQueue<Integer> storage = new ArrayBlockingQueue<>(50);
@@ -20,12 +22,13 @@ public class WrongWayVolatileCannotStop {
         }
 
         producer.canceled = true;
+//        thread.interrupt();
         System.out.println("消费者不需要数据了，中断生产者的线程");
 
     }
 }
 
-class Producer implements Runnable{
+class Producer implements Runnable {
     public volatile boolean canceled;
     private BlockingQueue<Integer> storage;
 
@@ -35,17 +38,17 @@ class Producer implements Runnable{
 
     @Override
     public void run() {
-        while (!canceled){
-            try {
+        try {
+            while (!canceled) {
                 storage.put(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
 
-class Consumer{
+class Consumer {
     private BlockingQueue<Integer> storage;
 
     public BlockingQueue<Integer> getStorage() {
@@ -56,7 +59,7 @@ class Consumer{
         this.storage = storage;
     }
 
-    public boolean isNeedMore(){
+    public boolean isNeedMore() {
         return Math.random() < 0.95;
     }
 }
